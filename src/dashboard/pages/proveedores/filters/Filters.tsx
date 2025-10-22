cd200734e214bbb3f7ded9ae8eb4836f8c68d03a
luisiy01 <luisnunez91@gmail.com>
2025-10-22 11:32:02 -0600

    actualizar filtros

--- src/dashboard/pages/proveedores/filters/Filters.tsx
+++ src/dashboard/pages/proveedores/filters/Filters.tsx
@@ -1,11 +1,4 @@
-import {
-  Checkbox,
-  FormControlLabel,
-  FormGroup,
-  Grid,
-  TextField,
-} from "@mui/material";
-import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
+import { Grid, TextField } from "@mui/material";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
@@ -13,7 +6,7 @@ import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 export const Filters = () => {
   return (
     <>
-      <Grid size={2}>
+      <Grid size={3}>
         <TextField
           variant="outlined"
           margin="normal"
@@ -24,6 +17,7 @@ export const Filters = () => {
           autoFocus
         />
       </Grid>
+      <Grid size={1} />
       <Grid size={3}>
         <TextField
           variant="outlined"
@@ -34,6 +28,7 @@ export const Filters = () => {
           name="alias"
         />
       </Grid>
+      <Grid size={1} />
       <Grid size={3}>
         <TextField
           variant="outlined"
@@ -44,17 +39,8 @@ export const Filters = () => {
           name="razonSocial"
         />
       </Grid>
-      <Grid size={2}>
-        <FormGroup>
-          <FormControlLabel control={<Checkbox />} label="CSF" />
-        </FormGroup>
-      </Grid>
-      <Grid size={2}>
-        <FormGroup>
-          <FormControlLabel control={<Checkbox />} label="Poder Rep. Legal" />
-        </FormGroup>
-      </Grid>
-      <Grid size={2}>
+
+      <Grid size={3}>
         <LocalizationProvider dateAdapter={AdapterDayjs}>
           <DatePicker
             sx={{ width: "100%" }}
@@ -63,6 +49,7 @@ export const Filters = () => {
           />
         </LocalizationProvider>
       </Grid>
+      <Grid size={1} />
       <Grid size={3}>
         <LocalizationProvider dateAdapter={AdapterDayjs}>
           <DatePicker
@@ -72,6 +59,7 @@ export const Filters = () => {
           />
         </LocalizationProvider>
       </Grid>
+      <Grid size={1} />
       <Grid size={3}>
         <LocalizationProvider dateAdapter={AdapterDayjs}>
           <DatePicker
@@ -80,23 +68,6 @@ export const Filters = () => {
             format="DD-MM-YYYY"
           />
         </LocalizationProvider>
-      </Grid>
-
-      <Grid size={2}>
-        <FormGroup>
-          <FormControlLabel
-            control={<Checkbox />}
-            label="Identificacion Rep. Legal"
-          />
-        </FormGroup>
-      </Grid>
-      <Grid size={2}>
-        <FormGroup>
-          <FormControlLabel
-            control={<Checkbox />}
-            label="Comprobante Domicilio"
-          />
-        </FormGroup>
       </Grid>
     </>
   );
