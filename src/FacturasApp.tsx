396423e1621358d2969daf646a03e5c9ff273f0a
luisiy01 <luisnunez91@gmail.com>
2025-10-28 10:07:30 -0600

    agregar theme

--- src/FacturasApp.tsx
+++ src/FacturasApp.tsx
@@ -1,12 +1,16 @@
 import { BrowserRouter } from "react-router";
 import { Toaster } from "sonner";
 import { FacturasRoutes } from "./routes/FacturasRoutes";
+import { ThemeProvider } from "@mui/material/styles";
+import { customTheme } from "./theme/theme";
 
 export const FacturasApp = () => {
   return (
     <BrowserRouter>
       <Toaster richColors />
-      <FacturasRoutes />
+      <ThemeProvider theme={customTheme}>
+        <FacturasRoutes />
+      </ThemeProvider>
     </BrowserRouter>
   );
 };
