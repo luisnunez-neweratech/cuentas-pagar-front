0b11f88013433216a07e7db65e737fd74fd0e5ba
luisiy01 <luisnunez91@gmail.com>
2025-10-29 14:13:33 -0600

    actualizar validacion de email

--- src/dashboard/pages/proveedor/contrato/components/steps/Contacto/components/Validations.ts
+++ src/dashboard/pages/proveedor/contrato/components/steps/Contacto/components/Validations.ts
@@ -4,5 +4,8 @@ export const validationSchema = yup.object().shape({
   tipoContacto: yup.string().required("Tipo Contacto es requerido"),
   contacto: yup.string().required("Contacto es requerido"),
   telefono: yup.string().required("Telefono es requerido"),
-  email: yup.string().email().required("Email es requerido"),
+  email: yup
+    .string()
+    .email("Tiene que ser un Email valido")
+    .required("Email es requerido"),
 });
