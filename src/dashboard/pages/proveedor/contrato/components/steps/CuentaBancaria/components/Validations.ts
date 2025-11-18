a587f7bae9ffa65b126221ebb20d00fb123ad610
luisiy01 <luisnunez91@gmail.com>
2025-11-18 10:54:10 -0600

    validar codigo swift

--- src/dashboard/pages/proveedor/contrato/components/steps/CuentaBancaria/components/Validations.ts
+++ src/dashboard/pages/proveedor/contrato/components/steps/CuentaBancaria/components/Validations.ts
@@ -2,8 +2,11 @@ import * as yup from "yup";
 
 const SUPPORTED_FORMATS = ["application/pdf"];
 
-export const validationSchema = (idInput: string, downloadUrl?: string | null) => { 
-  console.log('valid downloadUrl', downloadUrl) 
+export const validationSchema = (
+  idInput: string,
+  downloadUrl?: string | null
+) => {
+  console.log("valid downloadUrl", downloadUrl);
   if (downloadUrl) {
     return yup.object().shape({
       banco: yup.string().required("Banco es requerido"),
@@ -16,6 +19,10 @@ export const validationSchema = (idInput: string, downloadUrl?: string | null) =
       condicionesPago: yup
         .string()
         .required("Condiciones de Pago es requerido"),
+      swift: yup
+        .string()
+        .min(8, "Mínimo 8 dígitos")
+        .max(11, "Máximo 11 dígitos"),
     });
   }
 
@@ -36,5 +43,6 @@ export const validationSchema = (idInput: string, downloadUrl?: string | null) =
         "Solo archivos con formato .pdf",
         (value: any) => value && SUPPORTED_FORMATS.includes(value.type)
       ),
+    swift: yup.string().min(8, "Mínimo 8 dígitos").max(11, "Máximo 11 dígitos"),
   });
 };
