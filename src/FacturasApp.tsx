80f4073a6bc49e8b5e897f0756ca21d1029fc6df
Daniel Aguilar <Daniel.AguilarAvila@regalrexnord.com>
2025-11-25 16:10:17 -0600

    CI/CD Dev

--- src/FacturasApp.tsx
+++ src/FacturasApp.tsx
@@ -8,7 +8,7 @@ import { TanStackProvider } from "./plugins/TanStackProvider";
 export const FacturasApp = () => {
   return (
     <TanStackProvider>
-      <BrowserRouter>
+      <BrowserRouter basename="/Accounts_Payable">
         <Toaster richColors />
         <ThemeProvider theme={customTheme}>
           <FacturasRoutes />
