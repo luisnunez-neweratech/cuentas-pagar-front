2082558d74afb9348da4f6e193ec105a162b69ea
luisiy01 <luisnunez91@gmail.com>
2025-11-27 12:07:35 -0600

    agregar footer

--- src/dashboard/pages/factura/FacturaPage.tsx
+++ src/dashboard/pages/factura/FacturaPage.tsx
@@ -1,12 +1,14 @@
 import { Grid } from "@mui/material";
 import { FacturaHeader } from "./components/facturaHeader/FacturaHeader";
 import { FacturaDetalle } from "./components/facturaDetalle/FacturaDetalle";
+import { FacturaFooter } from "./components/facturaFooter/FacturaFooter";
 
 export const FacturaPage = () => {
   return (
     <Grid container spacing={2}>
       <FacturaHeader />
       <FacturaDetalle />
+      <FacturaFooter />
     </Grid>
   );
 };
