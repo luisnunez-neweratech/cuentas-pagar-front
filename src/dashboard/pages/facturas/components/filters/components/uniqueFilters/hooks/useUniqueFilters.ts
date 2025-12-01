d7bcbfbdebe88ba2f56eeacb387e3f8aedc80d95
luisiy01 <luisnunez91@gmail.com>
2025-12-01 11:56:51 -0600

    fix filters

--- src/dashboard/pages/facturas/components/filters/components/uniqueFilters/hooks/useUniqueFilters.ts
+++ src/dashboard/pages/facturas/components/filters/components/uniqueFilters/hooks/useUniqueFilters.ts
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import { getAllMonedaVentas } from "../../../../../../catalogos/services/monedaVenta.service";
 import { useEffect, useState } from "react";
+import { useFormik } from "formik";
 
 export const useUniqueFilters = () => {
   const [convertMonedas, setConvertMonedas] = useState<
@@ -23,7 +24,54 @@ export const useUniqueFilters = () => {
     setConvertMonedas(newMonedas ?? []);
   }, [monedas]);
 
+   const initialFormValues = () => {
+    /*     if (proveedorOcasional) {
+      if (proveedorOcasional.tipoProveedor === TipoProveedor.Contrato.value) {
+        navigate(`/proveedor/contrato/${id}`);
+      }
+      const productos = giros?.filter((obj) =>
+        proveedorOcasional.productos.includes(obj.id)
+      );
+      return {
+        tipoEntidad: proveedorOcasional.tipoEntidad,
+        tipoPersona: proveedorOcasional.tipoPersona,
+        rfc: proveedorOcasional.rfc,
+        razonSocial: proveedorOcasional.razonSocial,
+        alias: proveedorOcasional.alias ?? "",
+        email: proveedorOcasional.email,
+        productos: productos,
+      };
+    } */
+    return {
+      documentoId: null,
+      monedaId: null
+    };
+  };
+
+
+    const {
+      //handleSubmit,
+      values,
+      handleChange,
+      handleBlur,
+      touched,
+      errors,
+      //setFieldValue,
+    } = useFormik({
+      enableReinitialize: true,
+      initialValues: initialFormValues(),
+      validationSchema: null, //validationSchema,
+      onSubmit: async (_values) => {
+        //handleDisableButtons(true);
+      },
+    });
+
   return {
     convertMonedas,
+    values,
+    handleChange,
+    handleBlur,
+    touched,
+    errors
   };
 };
