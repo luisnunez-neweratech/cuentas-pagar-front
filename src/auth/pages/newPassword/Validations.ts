e13c3f2388945bb596ceec05eb514ad13735f5d3
luisiy01 <luisnunez91@gmail.com>
2026-01-13 08:22:20 -0600

    cambiar login a español

--- src/auth/pages/newPassword/Validations.ts
+++ src/auth/pages/newPassword/Validations.ts
@@ -1,8 +1,8 @@
 import * as yup from "yup";
 
 export const validationSchema = yup.object({
-  password: yup.string().required("Password is required"),
+  password: yup.string().required("Nueva contraseña es requerida"),
   newPassword: yup
     .string()
-    .oneOf([yup.ref("password")], "Passwords must match"),
+    .oneOf([yup.ref("password")], "Las contraseñas deben coincidir"),
 });
--- src/auth/pages/recoverPassword/Validations.ts
+++ src/auth/pages/recoverPassword/Validations.ts
@@ -1,5 +1,5 @@
 import * as yup from "yup";
 
 export const validationSchema = yup.object({
-  email: yup.string().email().required("Email if required"),
+  email: yup.string().email().required("Correo electrónico es requerido"),
 });
