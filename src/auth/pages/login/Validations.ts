bd7aabb218fdefa67160e774f289521769a128c1
luisiy01 <luisnunez91@gmail.com>
2026-01-13 07:16:14 -0600

    cambiar login a español

--- src/auth/pages/login/Validations.ts
+++ src/auth/pages/login/Validations.ts
@@ -1,6 +1,6 @@
 import * as yup from "yup";
 
 export const validationSchema = yup.object({
-  email: yup.string().email().required("Email is required"),
-  password: yup.string().required("Password is required"),
+  email: yup.string().email().required("Correo electrónico es requerido"),
+  password: yup.string().required("Contraseña es requerida"),
 });
