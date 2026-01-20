9000d072f380872c082f3b690e6fd285c010dab1
luisiy01 <luisnunez91@gmail.com>
2026-01-20 08:56:58 -0600

    add mutations file at invoices

--- src/dashboard/pages/factura/components/tabHeader/hooks/useTabHeader.ts
+++ src/dashboard/pages/factura/components/tabHeader/hooks/useTabHeader.ts
@@ -3,21 +3,16 @@ import { validationSchema } from "../../../Validations";
 import { useFacturaStore } from "../../../store/Factura.store";
 import type { Item } from "../../../../../../components/common/AutoComplete/interfaces/Item";
 import { useNavigate, useParams } from "react-router";
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import {
-  addFacturaDetalle,
-  addFacturaHeader,
-  updateFacturaDetalle,
-  updateFacturaHeader,
-  uploadFacturaFiles,
   getCheckDuplicate,
   calculateScheduledPaymentDate,
 } from "../../../services/factura.service";
 import { useEffect, useState } from "react";
 import { toast } from "sonner";
-import { AxiosError } from "axios";
 import { useDashboardLayoutStore } from "../../../../../store/dashboardLayout.store";
 import { useQueries } from "./useQueries";
+import { useMutations } from "./useMutations";
 
 interface props {
   onClickGuardar: number;
@@ -53,7 +48,7 @@ export const useTabHeader = ({ onClickGuardar }: props) => {
   );
 
   const setTipoEntidadId = useFacturaStore((state) => state.setTipoEntidadId);
-  const setFacturaId = useFacturaStore((state) => state.setFacturaId);
+
   const setDisableButtons = useFacturaStore((state) => state.setDisableButtons);
   const setPdfDownloadUrl = useFacturaStore((state) => state.setPdfDownloadUrl);
   const setXmlDownloadUrl = useFacturaStore((state) => state.setXmlDownloadUrl);
@@ -78,6 +73,17 @@ export const useTabHeader = ({ onClickGuardar }: props) => {
     setIsLoading(state);
   };
 
+  const {
+    createMutationDetalle,
+    uploadDocumentosMutation,
+    createMutation,
+    updateHeaderMutation,
+    updateMutationDetalle,
+  } = useMutations({
+    handleDisableButtons,
+    navigate,
+  });
+
   const handleOpenModal = useFacturaStore((state) => state.handleOpenModal);
 
   const [listaProductos, setListaProductos] = useState<
@@ -96,143 +102,6 @@ export const useTabHeader = ({ onClickGuardar }: props) => {
 
   const [callCheckDuplicateFactura, setCallCheckDuplicateFactura] =
     useState<boolean>(false);
-
-  const createMutationDetalle = useMutation({
-    mutationFn: addFacturaDetalle,
-    onSuccess: () => {
-      toast.success("Factura creada correctamente");
-      handleDisableButtons(false);
-      navigate("/facturas");
-    },
-    onError: (error) => {
-      console.log(error);
-      if (error instanceof AxiosError) {
-        if (error.response) {
-          toast.error(error.response.data);
-          return;
-        }
-        toast.error(error.message);
-        return;
-      }
-      toast.error("Error al agregar el detalle de la factura");
-      return;
-    },
-    onSettled: () => {
-      handleDisableButtons(false);
-    },
-  });
-
-  const uploadDocumentosMutation = useMutation({
-    mutationFn: uploadFacturaFiles,
-    onError: (error) => {
-      console.log(error);
-      if (error instanceof AxiosError) {
-        toast.error(error.message);
-        return;
-      }
-      toast.error("Error al subir los documentos");
-      return;
-    },
-    onSettled: () => {
-      handleDisableButtons(false);
-    },
-  });
-
-  const createMutation = useMutation({
-    mutationFn: addFacturaHeader,
-    onSuccess: (data) => {
-      setFacturaId(data.data.id); //id header creado, por si falla el detalle usar este id
-      const newDetalles = (stateFactura.facturaDetalle ?? []).map((detalle) => {
-        return {
-          id: 0,
-          lineNumber: 0,
-          quantity: +detalle.cantidad,
-          productServiceKey: detalle.codigo.toString(),
-          concept: detalle.concepto.toString(),
-          unitPrice: +detalle.precio,
-          lineDiscount: 0,
-          lineTotal: +detalle.total,
-          uofMId: detalle.uMedida,
-        };
-      });
-      createMutationDetalle.mutate({
-        invoiceId: data.data.id,
-        postFacturaDetallePayload: newDetalles,
-      });
-
-      if (
-        stateFactura.xmlFileValue ||
-        stateFactura.pdfFileValue ||
-        stateFactura.paymentProofFileValue
-      ) {
-        uploadDocumentosMutation.mutate({
-          facturaId: data.data.id,
-          xml: stateFactura.xmlFileValue,
-          pdf: stateFactura.pdfFileValue,
-          paymentProof: stateFactura.paymentProofFileValue,
-        });
-      }
-    },
-    onError: (error) => {
-      console.log(error);
-      if (error instanceof AxiosError) {
-        if (error.response) {
-          toast.error(error.response.data);
-          return;
-        }
-        toast.error(error.message);
-        return;
-      }
-      toast.error("Error al agregar la factura");
-      return;
-    },
-    onSettled: () => {
-      handleDisableButtons(false);
-    },
-  });
-
-  const updateHeaderMutation = useMutation({
-    mutationFn: updateFacturaHeader,
-    onSuccess: () => {
-      //toast.success("Factura actualizada correctamente");
-    },
-    onError: (error) => {
-      console.log(error);
-      if (error instanceof AxiosError) {
-        toast.error(error.message);
-        return;
-      }
-      toast.error("Error al actualizar la factura");
-      return;
-    },
-    onSettled: () => {
-      //handleDisableButtons(false);
-    },
-  });
-
-  const updateMutationDetalle = useMutation({
-    mutationFn: updateFacturaDetalle,
-    onSuccess: () => {
-      toast.success("Factura actualizada correctamente");
-      navigate("/facturas");
-    },
-    onError: (error) => {
-      console.log(error);
-      if (error instanceof AxiosError) {
-        if (error.response) {
-          toast.error(error.response.data);
-          return;
-        }
-        toast.error(error.message);
-        return;
-      }
-      toast.error("Error al agregar el detalle de la factura");
-      return;
-    },
-    onSettled: () => {
-      handleDisableButtons(false);
-    },
-  });
 
   const initialFormValues = () => {
     if (id && facturaBD && proveedores && colaboradores) {
