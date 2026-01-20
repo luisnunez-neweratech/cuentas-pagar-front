44856d495e052e20bf2f520fe49a518e09013339
luisiy01 <luisnunez91@gmail.com>
2026-01-20 10:26:03 -0600

    refactor token login

--- src/auth/pages/recoverToken/hooks/useRecoverToken.ts
+++ src/auth/pages/recoverToken/hooks/useRecoverToken.ts
@@ -1,28 +1,20 @@
 import { useNavigate, useSearchParams } from "react-router";
 import { useFormik } from "formik";
 import { validationSchema } from "../Validations";
-import { useMutation } from "@tanstack/react-query";
-import { verifyToken } from "../../../services/recoverToken.action";
-import { toast } from "sonner";
 import { useEffect } from "react";
 import { useAuthLayoutStore } from "../../../store/authLayout.store";
-import { axiosErrorMessage } from "../../../../lib/axiosError";
+import { useMutations } from "./useMutations";
 
 export const useRecoverToken = () => {
   let [searchParams, _setSearchParams] = useSearchParams();
   const setIsLoading = useAuthLayoutStore((state) => state.setIsLoading);
-  const verifyTokenMutation = useMutation({
-    mutationFn: verifyToken,
-    onSuccess: () => {
-      toast.info("Token Correcto");
-      navigate(`/auth/new-password?email=${searchParams.get("email")}`);
-    },
-    onError: (error) => {
-      toast.error(axiosErrorMessage(error, "Token incorrecto"));
-    },
+  const navigate = useNavigate();
+
+  const { verifyTokenMutation } = useMutations({
+    navigate,
+    email: searchParams.get("email") ?? "",
   });
 
-  const navigate = useNavigate();
   const { handleSubmit, values, handleChange, handleBlur, touched, errors } =
     useFormik({
       initialValues: {
