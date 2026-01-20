cb51b74ac30dfc2ae59d69e35c5fa7e831b40684
luisiy01 <luisnunez91@gmail.com>
2026-01-20 10:16:58 -0600

    refactor login

--- src/auth/pages/login/hooks/useLoginPage.ts
+++ src/auth/pages/login/hooks/useLoginPage.ts
@@ -2,35 +2,12 @@ import { useEffect } from "react";
 import { useFormik } from "formik";
 import { toast } from "sonner";
 import { validationSchema } from "../Validations";
-import { useAuthStore } from "../../../../stores/auth/auth.store";
-import { useMutation } from "@tanstack/react-query";
-import { loginAction } from "../../../services/login.action";
-import type { User } from "../../../../interfaces/user.interface";
 import { useAuthLayoutStore } from "../../../store/authLayout.store";
-import { axiosErrorMessage } from "../../../../lib/axiosError";
+import { useMutations } from "./useMutations";
 
 export const useLoginPage = () => {
-  const loginUser = useAuthStore((state) => state.loginUser);
+  const { loginMutation } = useMutations();
   const setIsLoading = useAuthLayoutStore((state) => state.setIsLoading);
-
-  const loginMutation = useMutation({
-    mutationFn: loginAction,
-    onSuccess: (login) => {
-      const { token, mail, nickName, name, colaboratorId } = login;
-      //mapper
-      const user: User = {
-        id: colaboratorId,
-        email: mail,
-        fullName: name,
-        roles: ["Admin"], // TODO backend regrese los roles, por ahora todos son admin
-        nickName,
-      };
-      loginUser(token, user);
-    },
-    onError: (error: Error) => {
-      toast.error(axiosErrorMessage(error, "Correo o contraseña incorrecta"));
-    },
-  });
 
   useEffect(() => {
     setIsLoading(loginMutation.isPending);
@@ -48,7 +25,7 @@ export const useLoginPage = () => {
           loginMutation.mutate({ email, password });
           return;
         } catch (error) {
-          console.log("no se pudo autenticar");
+          console.error('error', error);
           toast.error("Correo o contraseña incorrecta ");
         }
       },
