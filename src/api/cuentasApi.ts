ae0117497ea7be5e920280fb4e647aaec09f95e4
luisiy01 <luisnunez91@gmail.com>
2026-01-21 12:28:23 -0600

    logout user on 401 error

--- src/api/cuentasApi.ts
+++ src/api/cuentasApi.ts
@@ -1,9 +1,10 @@
 import axios from "axios";
+import { useAuthStore } from "../stores/auth/auth.store";
 
 const cuentasApi = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   paramsSerializer: {
-    indexes: null, 
+    indexes: null,
   },
 });
 
@@ -14,5 +15,18 @@ cuentasApi.interceptors.request.use((config) => {
   }
   return config;
 });
+
+cuentasApi.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    const logoutUser = useAuthStore((state) => state.logoutUser);
+    if (error.response.status === 401) {
+      logoutUser();
+    }
+    return error;
+  },
+);
 
 export { cuentasApi };
