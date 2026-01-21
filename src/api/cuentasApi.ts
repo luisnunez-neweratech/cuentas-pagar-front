aedebc436ad588667780160bfc957a0351fe6583
luisiy01 <luisnunez91@gmail.com>
2026-01-21 13:16:57 -0600

    add axios instance

--- src/api/cuentasApi.ts
+++ src/api/cuentasApi.ts
@@ -1,5 +1,4 @@
 import axios from "axios";
-import { useAuthStore } from "../stores/auth/auth.store";
 
 const cuentasApi = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
@@ -15,18 +14,5 @@ cuentasApi.interceptors.request.use((config) => {
   }
   return config;
 });
-
-cuentasApi.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  (error) => {
-    const logoutUser = useAuthStore((state) => state.logoutUser);
-    if (error.response.status === 401) {
-      logoutUser();
-    }
-    return error;
-  },
-);
 
 export { cuentasApi };
